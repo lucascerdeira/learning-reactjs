d83fd475226d2eb294c7a2c726dbcc11c760688c
lcerdeira <lucas.cerdeira@segurossura.com.br>
2024-10-01 20:08:45 -0300

    aprendendo react

---
+++ gerenciador-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo! React</h1>
+      <p>Meu primeiro projeto com React</p>
+    </div>
+  );
+}
+
+export default App;
